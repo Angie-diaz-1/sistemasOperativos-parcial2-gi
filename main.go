5e2bd0714e9a1c84be56e1eed8f405dc74f6bcde
Angie-diaz-1 <diaz.angie.2352@eam.edu.co>
2025-05-04 14:50:39 -0500

    test action 9

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"os"
 	"usersProject/controller"
 	"usersProject/database"
@@ -12,10 +10,12 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hola Docker!")
-	})
+	r := gin.Default()
 
+	// Endpoint de salud
+	r.GET("/health", func(c *gin.Context) {
+		c.String(200, "Hola Docker!")
+	})
 	db := database.ConnectMongoDB()
 
 	collectionName := os.Getenv("MONGO_COLLECTION_NAME")
@@ -24,7 +24,6 @@ func main() {
 	userService := service.NewUserService(userRepo)
 	userController := controller.NewUserController(userService)
 
-	r := gin.Default()
 	userController.RegisterRoutes(r)
 
 	r.Run(":8080") // Corre en localhost:8080
